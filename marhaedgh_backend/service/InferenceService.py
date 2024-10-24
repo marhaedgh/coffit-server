a790d7a207c0f5760f82c843edbc8ae30fab1231
WagyuShark <einokt@naver.com>
2024-10-24 17:26:35 +0000

    sendInferenceRequest 서비스레이어 구현

--- marhaedgh_backend/service/InferenceService.py
+++ marhaedgh_backend/service/InferenceService.py
@@ -1,6 +1,8 @@
 import asyncio
 from vllm import AsyncEngineArgs, AsyncLLMEngine, SamplingParams
 
+from dto.InferResponseDto import InferResponseDto
+
 class InferenceService:
 
     def __init__(self, modelLoader):
@@ -11,7 +13,7 @@ class InferenceService:
         #gRPC Client ver
         return 0
 
-    def sendInferenceRequest_vLLM(self):
+    async def sendInferenceRequest_vLLM(self, role, content):
         # vllm serving ver
         sampling_params = SamplingParams(
             temperature=0.0,
@@ -19,26 +21,12 @@ class InferenceService:
             stop_token_ids=self.modelLoader.stop_tokens(),
         )
 
-        conversation = [{"role": "user", "content": "너가 평가하기에 너는 어느정도의 한국어 실력을 가진 것 같아?"}]
+        conversation = [{"role": role, "content": content}]
+
         chat = self.modelLoader.tokenizer.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
-        result = asyncio.run(self.modelLoader.run_single(chat, "123"))
-        print(result)
+        result = await self.modelLoader.run_single(chat, "0", sampling_params) #userid로 requestid 식별해도 괜찮겠다
 
-        # Runs multiple inferences in parallel
-        conversations = [
-            [{"role": "user", "content": "세종대왕 맥북 던짐 사건에 대해 말해줘"}],
-            [{"role": "user", "content": "2341+12316은 뭐야?"}],
-            [{"role": "user", "content": "한국에서 내야하는 세금의 종류에 대해서 알려줘."}],
-            [{"role": "user", "content": "개인사업자가 내야하는 특별한 세금이 있을까?"}],
-            [{"role": "user", "content": "내가 대회에서 우승할 수 있는 확률을 계산하는 법을 알려줘."}],
-        ]
-        chats = [
-            self.modelLoader.tokenizer.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
-            for conversation in conversations
-        ]
-        results = asyncio.run(self.modelLoader.run_multi(chats))
-        print(results)
+        inferResponseDto = InferResponseDto(**{'result': result.outputs[0].text})
 
-        return 0
-
+        return inferResponseDto
 
