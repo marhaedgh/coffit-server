a6902797096e4ed4b5116d9e492ee1e68a8a9795
WagyuShark <einokt@naver.com>
2024-10-24 12:29:49 +0000

    inference, user service add

---
+++ marhaedgh_backend/service/InferenceService.py
@@ -0,0 +1,16 @@
+
+
+class InferenceService:
+
+    def sendInferenceRequest_gRPC(self):
+
+        #gRPC Client ver
+        return 0
+
+    def sendInferenceRequest_vLLM(self):
+        # vllm serving ver
+
+        # - app() 에서 모델 생성하고 받아야되는거 아님?
+        return 0
+
+
---
+++ marhaedgh_backend/service/UserService.py
@@ -0,0 +1,8 @@
+
+
+class UserService:
+
+    def test(self):
+
+        #회원가입 후 로그인 절차가 똑같음. 일단 코드 중복해서 구현
+        return 0
