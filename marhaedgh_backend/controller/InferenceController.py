24a57bf934d1b9332fc08e08f5d55b99aa883d21
WagyuShark <einokt@naver.com>
2024-10-24 15:48:51 +0000

    fix import and implement inferenceRequest

--- marhaedgh_backend/controller/InferenceController.py
+++ marhaedgh_backend/controller/InferenceController.py
@@ -2,13 +2,19 @@ from fastapi import APIRouter, HTTPException
 
 from service.InferenceService import InferenceService
 
+import ModelLoader
+
+#모델 불러오기
+modelLoader = ModelLoader.InferenceModel()
+inferenceService = InferenceService(modelLoader)
+
 router = APIRouter(
-    prefix="/api/v1/",
+    prefix="/api/v1",
 )
 
 @router.post("/infer")
 async def inferenceRequest():
 
-
-
-    return result;
+    inferenceService.sendInferenceRequest_vLLM()
+    print("안녕 실행 잘 됐어@@@@@@@@@@@@@@@@@@@@@2")
+    return 0
