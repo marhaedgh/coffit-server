47d50c8955a4104d063e7f9529b644c5c159cd87
WagyuShark <einokt@naver.com>
2024-10-24 12:30:15 +0000

    create main.py

---
+++ marhaedgh_backend/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+#from controller
+
+SWAGGER_HEADERS = {
+    "title": "말해다규현 api 서버 테스트",
+    "version": "0.1.0",
+    "description": "## 말해다규현 api 서버 입니다.\n",
+    "contact": {
+        "name": "wagyu",
+    },
+}
+app = FastAPI(
+    swagger_ui_parameters={
+        "deepLinking": True,
+        "displayRequestDuration": True,
+        "docExpansion": "none",
+        "operationsSorter": "method",
+        "filter": True,
+    },
+    **SWAGGER_HEADERS
+)
+
+origins = [
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#vllm 모델 생성 코드
+
+
+app.include_router(InferenceController.router)
