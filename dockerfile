aa400714dd8de34d5bb8aa6e2bb8436239fbf2f2
WagyuShark <einokt@naver.com>
2024-11-01 11:34:28 +0000

    database 스키마 변경 및 한국어 이슈로 인한 mysql 버전 변경

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-FROM mysql:5.7
+FROM mysql:latest
 ENV  MYSQL_ROOT_PASSWORD=1234
 ENV  MYSQL_DATABASE=malhaedgh_db
 ADD  ./init/* /docker-entrypoint-initdb.d
--- init/init.sql
+++ init/init.sql
@@ -1,34 +1,42 @@
-CREATE DATABASE IF NOT EXISTS malhaedgh_db;
+CREATE DATABASE IF NOT EXISTS malhaedgh_db DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 USE malhaedgh_db;
 
 CREATE TABLE `users` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `username` VARCHAR(20),
-  `business_type` VARCHAR(15),
-  `corporation_type` VARCHAR(15),
-  `industry` VARCHAR(60),
-  `region` VARCHAR(40),
-  `representative_birthday` VARCHAR(12),
-  `representative_gender` VARCHAR(5),
-  `revenue` float,
-  `employees` int,
-  `created_at` timestamp
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `username` VARCHAR(255),
+  `business_data_id` INT,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `business_data` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `business_type` VARCHAR(255),
+  `corporation_type` VARCHAR(255),
+  `industry` VARCHAR(255),
+  `region` VARCHAR(255),
+  `representative_birthday` VARCHAR(10),
+  `representative_gender` VARCHAR(10),
+  `revenue` FLOAT,
+  `employees` INT
 );
 
 CREATE TABLE `alerts` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `business_data_id` INT,
   `title` VARCHAR(255),
   `keywords` JSON,
   `line_summarization` VARCHAR(255),
-  `text_summarization` text,
-  `task_summarization` text,
-  `detail_report` text
+  `text_summarization` TEXT,
+  `task_summarization` TEXT,
+  `detail_report` TEXT,
+  `due_date` TIMESTAMP
 );
 
 CREATE TABLE `user_alert_mapping` (
-  `user_id` integer,
-  `alert_id` integer
+  `user_id` INT,
+  `alert_id` INT,
+  PRIMARY KEY (`user_id`, `alert_id`)
 );
 
 CREATE INDEX idx_user_id ON user_alert_mapping (user_id);
