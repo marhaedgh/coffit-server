a1ebbf4f9c3c46e655bf252dcf22d2fb8e36bacd
WagyuShark <einokt@naver.com>
2024-11-01 18:58:05 +0000

    line summarization 부분 추가

--- marhaedgh_backend/dto/DemonInferResponse.py
+++ marhaedgh_backend/dto/DemonInferResponse.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 class DemonInferResponse(BaseModel):
     title: str
     keywords: str
+    line_summarization: str
     summarization: str
     classification: str
     what_to_do: str
