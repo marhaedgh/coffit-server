b72f07d40d147e60498cfad43e2ada96feb320ce
WagyuShark <einokt@naver.com>
2024-11-09 16:15:18 +0000

    Streamlit 채팅 구현 및 fastapi 마운트

--- marhaedgh_backend/service/InferenceService.py
+++ marhaedgh_backend/service/InferenceService.py
@@ -30,3 +30,27 @@ class InferenceService:
 
         return inferResponse
 
+
+    async def inference_chatting_streaming(self, messages):
+        sampling_params = SamplingParams(
+            temperature=0.0,
+            skip_special_tokens=True,
+            stop_token_ids=self.modelLoader.stop_tokens(),
+        )
+
+        # 이전 출력된 텍스트 길이를 추적
+        previous_length = 0
+
+        chats = self.modelLoader.tokenizer.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
+
+        async for result in self.modelLoader.engine.generate(chats, sampling_params, request_id="chat0"):
+            # 현재 생성된 전체 텍스트 가져오기
+            current_text = result.outputs[0].text
+            
+            # 새로운 텍스트 부분만 추출
+            new_text = current_text[previous_length:]
+            previous_length = len(current_text)
+
+            # 새로운 부분만 출력
+            if new_text.strip():  # 새로운 텍스트가 존재하면 출력
+                yield new_text + "\n"
