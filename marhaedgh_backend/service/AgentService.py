4cb1fb6f3169950d0fa5e27f812291406ff2d048
WagyuShark <einokt@naver.com>
2024-11-11 19:16:17 +0000

    추론서버 분리에 따른 agent Service 코드 수정 및 적용

--- marhaedgh_backend/service/AgentService.py
+++ marhaedgh_backend/service/AgentService.py
@@ -4,6 +4,8 @@ from vllm import AsyncEngineArgs, AsyncLLMEngine, SamplingParams
 from langchain.text_splitter import CharacterTextSplitter
 from sqlalchemy.orm import Session
 
+from llama_index.core import Settings
+
 from dto.DemonInferResponse import DemonInferResponse
 from db.database import get_db
 from repository.BusinessDataRepository import BusinessDataRepository
@@ -34,46 +36,29 @@ class AgentService:
     async def demon_alert_response_efficient(self, url):
         #URL 타고 들어가서 본문내용(PDF) 추출 후 텍스트로 변환 //일단 html스크랩
 
-        output_dir = "./rag_data"  # 저장할 디렉토리 경로
-        # URL에서 텍스트 추출 및 저장
+        output_dir = "./rag_data/data"
+
         content = extract_text_from_url(url, output_dir)
 
-        # 배치로 요청을 처리합니다.
-        sampling_params = SamplingParams(
-            temperature=0.0,
-            skip_special_tokens=True,
-            stop_token_ids=self.modelLoader.stop_tokens(),
-        )
 
-        # 각 추론 요청에 필요한 JSON 파일을 로드하고 메시지를 생성합니다.
-        layer0_tasks = [
-            await self.prepare_request(content, "./prompt/title.json"),                   #제목
-            await self.prepare_request(content, "./prompt/keywords.json"),                #키워드
-            await self.prepare_request(content, "./prompt/summarization_korean.json"),    #요약
-            await self.prepare_request(content, "./prompt/classification.json"),          #분류
-            await self.prepare_request(content, "./prompt/whattodo.json"),                #할일
-        ]
+        layer0_title = Settings.llm.complete(await self.prepare_request(content, "./prompt/title.json"))
+        layer0_keywords = Settings.llm.complete(await self.prepare_request(content, "./prompt/keywords.json"))
+        layer0_summarization = Settings.llm.complete(await self.prepare_request(content, "./prompt/summarization_korean.json"))
+        layer0_classification = Settings.llm.complete(await self.prepare_request(content, "./prompt/classification.json"))
+        layer0_whattodo = Settings.llm.complete(await self.prepare_request(content, "./prompt/whattodo.json"))
 
-        layer0_results = await self.modelLoader.run_multi(layer0_tasks, sampling_params)
 
-        layer1_tasks = [
-            await self.prepare_request(
-                layer0_results[2].outputs[0].text,
-                "./prompt/line_summarization.json"                               #요약 -> 한줄요약
-            )
-        ]
+        layer1_line_summarization = Settings.llm.complete(await self.prepare_request(content, "./prompt/line_summarization.json"))
 
-        layer1_results = await self.modelLoader.run_multi(layer1_tasks, sampling_params)
 
-        # 여기서 레이어링 / 멀티 단위로 해서 더 빠르게 처리
+        title = str(layer0_title)
+        keyword = str(layer0_keywords)
+        line_summarization = str(layer1_line_summarization)
+        summarization = str(layer0_summarization)
+        classification = str(layer0_classification)
+        whattodo = str(layer0_whattodo)
 
-        title = layer0_results[0].outputs[0].text
-        keyword = layer0_results[1].outputs[0].text
-        summarizations = [layer0_results[2].outputs[0].text, layer1_results[0].outputs[0].text]
-        classification = layer0_results[3].outputs[0].text
-        whattodo = layer0_results[4].outputs[0].text
 
-        # 데이터베이스에 저장
         db: Session = next(get_db())
         business_data_repository = BusinessDataRepository(db)
         business_data_id = 0  # 예시값
@@ -83,8 +68,8 @@ class AgentService:
             "business_data_id": business_data_id,
             "title": title,
             "keywords": json.dumps({"keywords": keyword}),
-            "line_summarization": summarizations[1],
-            "text_summarization": summarizations[0],
+            "line_summarization": line_summarization,
+            "text_summarization": summarization,
             "task_summarization": whattodo
         })
 
@@ -94,14 +79,16 @@ class AgentService:
             "alert_id": gen_alert.id
         })
 
+
         demon_infer_response = DemonInferResponse(
             title=title, 
             keywords=keyword,
-            line_summarization=summarizations[1],
-            summarization=summarizations[0],
+            line_summarization=line_summarization,
+            summarization=summarization,
             classification=classification,
             what_to_do=whattodo
         )
+
 
         return demon_infer_response
         
