ab9e3de0555cdeb96e8a52e4fdf93f267535d81a
WagyuShark <einokt@naver.com>
2024-11-14 17:14:04 +0000

    추론 요청 비동기 처리로 변경 및 엔드포인트명 변경

--- marhaedgh_backend/service/InferenceService.py
+++ marhaedgh_backend/service/InferenceService.py
@@ -14,33 +14,33 @@ class InferenceService:
         self.modelLoader = modelLoader
 
     async def sendInferenceRequest_vLLM(self, role, content):
-
+       
         conversation = [{"role": role, "content": content}]
 
         question = self.modelLoader.tokenizer.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
-
-        response = Settings.llm.complete(question)
+        
+        response = await Settings.llm.acomplete(question)
 
         inferResponse = InferResponse(**{'result': str(response)})
 
         return inferResponse
 
 
-    async def inference_chatting_streaming(self, messages):
+    async def inference_chatting_streaming(self, question, prompt):
 
-        #embedding해서 rag할 질문은 모든 message를 넣는게 아니라 질문만 정확히 넣어서 찾게 해야될 것 같은데... 수정 필요
-        chats = self.modelLoader.tokenizer.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
+        nodes = self.modelLoader.retriever.retrieve(question)
 
-        nodes = self.modelLoader.retriever.retrieve(chats)
+        chat = self.modelLoader.tokenizer.apply_chat_template(prompt, add_generation_prompt=True, tokenize=False)
 
         # Generate streaming response
         streaming_response = self.modelLoader.query_engine.synthesize(
-            chats,
+            chat,
             nodes=nodes
         )
-
-        def generate():
+        
+        def generator():
             for text in streaming_response.response_gen:
-                yield text + '\n'
+                print(text)
+                yield text
 
-        return generate()
+        return generator()
