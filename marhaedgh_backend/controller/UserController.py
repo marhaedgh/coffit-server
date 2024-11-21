2f6f24930a58db720b83820a6780c12042714eff
WagyuShark <einokt@naver.com>
2024-11-21 20:49:09 +0000

    UserController 로깅 추가

--- marhaedgh_backend/controller/UserController.py
+++ marhaedgh_backend/controller/UserController.py
@@ -1,5 +1,5 @@
+import logging
 from fastapi import APIRouter
-
 from dto.BaseResponse import BaseResponse
 from dto.CreateBusinessRequest import CreateBusinessRequest
 from dto.CreateBusinessResponse import CreateBusinessResponse
@@ -8,6 +8,13 @@ from service.AgentService import AgentService
 from service.UserService import UserService
 
 import ModelLoader
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger("API Logger")
 
 router = APIRouter(
     prefix="/api/v1/user",
@@ -20,7 +27,12 @@ agentService = AgentService(ModelLoader.InferenceModel())
 @router.post("/", response_model=BaseResponse)
 async def create_business(req: CreateBusinessRequest):
 
+    logger.info(f"Request Data (Create Business): {req.model_dump()}")
+
     create_business_response = userService.create_business(req)
+
     await agentService.initial_mapping_notifications(create_business_response)
+
+    logger.info(f"Response Data (Create Business): {create_business_response}")
 
     return BaseResponse(message="success - create_business", data=create_business_response)
